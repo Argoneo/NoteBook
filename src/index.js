647bd7bec5a079be830a643f175344a2ed0a211e
Argoneo <158490681+Argoneo@users.noreply.github.com>
2025-07-23 09:41:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./../public/styles.css";
 import "./styles.css";  // IMPORT corretto, CSS dentro src
 
 ReactDOM.render(<App />, document.getElementById("root"));
